e13ed883109947c66496e4169e406fbace81aef0
Omrani Mootez Belleh <114734614+Refereeing@users.noreply.github.com>
2023-01-16 14:27:28 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import './App.css';
+import UserList from './Components/UserList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+function App() {
+  return (
+    <div className="App-list">
+      <h1>The list of users</h1>
+      <UserList/>
+    </div>
+  );
+}
+
+export default App;
