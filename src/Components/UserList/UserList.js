01e33f8d82c3f83532338f440a2da183ea5e03f8
Omrani Mootez Belleh <114734614+Refereeing@users.noreply.github.com>
2023-01-16 14:30:40 +0100

    Add files via upload

--- src/Components/UserList/UserList.js
+++ src/Components/UserList/UserList.js
@@ -1,1 +1,36 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import User from './User';
 
+const UserList = () => {
+    
+    //let x = axios.get("https://jsonplaceholder.typicode.com/posts")
+                 //.then((res)=> console.log(res.data))
+                 //.catch((err)=> console.log(err));
+    //console.log(x);
+    
+    // where to store the returned data
+    const [listOfUser, setListOfUser]=useState ([])
+
+
+    //tell React we need to use an effect everytime the component render
+    //access the listOfUser state right from the effect
+    //it replaces component's lifecycle
+    useEffect( () => {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((res)=> setListOfUser(res.data))
+        .catch((err)=> console.log(err));
+
+    }, []);
+
+
+    return (
+    <div className="user-card">
+        {/*{listOfUser.map((user)=> console.log(user.name))};*/}
+        {listOfUser.map((scent)=> <User scent={scent}/>)}
+
+    </div>
+  )
+}
+
+export default UserList
